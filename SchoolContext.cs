240b8fb9d0568ab847849e90770f1ffaa17e92f7
sabahussains <140971557+sabahussains@users.noreply.github.com>
2024-02-02 13:25:13 -0500

    Add files via upload

---
+++ SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvcProject
+{
+    using FirstMvcProject.Models;
+    using System.Data.Entity;
+    using Microsoft.EntityFrameworkCore;
+
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Replace "YourConnectionString" with your actual SQL Server connection string
+            optionsBuilder.UseSqlServer("YourConnectionString");
+        }
+    }
+
+}
